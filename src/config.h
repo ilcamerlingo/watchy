4022dab86cab16f1e4569ba7a95e95989778919c
ilcamerlingo <87574224+ilcamerlingo@users.noreply.github.com>
2021-07-19 13:38:07 +0200

    Update config.h

--- src/config.h
+++ src/config.h
@@ -25,10 +25,10 @@
 #define DISPLAY_WIDTH 200
 #define DISPLAY_HEIGHT 200
 //weather api
-#define CITY_NAME "NEW+YORK" //if your city name has a space, replace with '+'
-#define COUNTRY_CODE "US"
-#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
-#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?q="
+#define CITY_NAME "LYON" //if your city name has a space, replace with '+'
+#define COUNTRY_CODE "FR"
+#define OPENWEATHERMAP_APIKEY "d2c9e4f677ea9d1c038ca8a685a5e6c1" //use your own API key :)
+#define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?q=LYON,fr&APPID=d2c9e4f677ea9d1c038ca8a685a5e6c1"
 #define TEMP_UNIT "metric" //use "imperial" for Fahrenheit"
 #define WEATHER_UPDATE_INTERVAL 30 //minutes
 //wifi
